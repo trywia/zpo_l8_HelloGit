a2ab8c707056069823136c7186d0ae8ebe03d275
trywi <249954@student.pwr.edu.pl>
2020-11-27 13:29:11 +0100

    First commit (pierwszy zapis)

---
+++ src/edu/Main.java
@@ -0,0 +1,11 @@
+package edu;
+
+public class Main {
+
+    // commit zgłoszenie plików do repozystorium
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Git");
+    }
+}
